05a2c6e018277421cebe18c721ca84e0e7c92363
MuhAtthar <muhatthar@gmail.com>
2023-04-02 00:52:40 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/remind/MainActivity.kt
+++ app/src/main/java/com/example/remind/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.remind
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,6 +14,7 @@ import com.example.remind.screen.onboard.RegisterScreen
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
